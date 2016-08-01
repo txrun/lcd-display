ca844b8abe3a5022ffd16e33f030d2ac9af7d175
Tarun Mathew <crushd@gmail.com>
2016-08-01 17:29:57 +0530

    Refactored function scale

--- lcdDisplay.js
+++ lcdDisplay.js
@@ -2,14 +2,20 @@
 var value = 148;
 var defaultSize = 1;
 
-function displayOne(){
-  var one = [
+const numberToCharacterMap = {
+  1 : [
     [' ',' ',' '],
     [' ',' ','|'],
     [' ',' ',' '],
     [' ',' ','|'],
-    [' ',' ',' ']];
-}
+    [' ',' ',' ']],
+  4 : [
+    [' ',' ',' '],
+    ['|',' ','|'],
+    [' ','-',' '],
+    [' ',' ','|'],
+    [' ',' ',' ']]
+};  
 
   var four = [
     [' ',' ',' '],
@@ -26,49 +32,60 @@ function displayOne(){
     [' ','-',' ']];
 
 
-function scale(array, size){
-  var scaledArray = [];
-  var spaces = [];
-  array.forEach(function(line, i, mainArray){
-    var flag = 0;
-    if(line.indexOf('|') === -1 && line.indexOf('-')=== -1){
-      spaces = ' '.repeat(line.length + size -2);
-      scaledArray.push(spaces.split(""));
-    }
-    else if (line.indexOf('|') >= 0) {
-      line.splice(line.length-1, 0, ' ');
-      scaledArray.push(line);
-      scaledArray.push(line);
-      
-      // mainArray.splice(i, 0, line);
-      // flag = 1;
-    }
-    else if (line.indexOf('-') > 0) {
-      line.splice(line.length-1, 0, '-');
-      scaledArray.push(line);
-    }
-
-    // if(flag === 1) {
-    //   mainArray.splice(i, 0, line);
-
-    // }
-  });
-
-  console.log(scaledArray);
-}
-
-function digitDisplay(digit){
-  digit.forEach(function(line, i, linesArray)
+function displayDigit(characterArrays) {
+  characterArrays.forEach(function(line, i, linesArray)
   {
     var ch = line.join(" ");
     console.log(ch);
-    // console.log(linesArray[i]);
   });
 }
 
-var displayDigit = digitDisplay(four);
-var scaleDigit = scale(four, 3);
-// var displayScaledDigit =  digitDisplay(scaleDigit);
+function scale(array, size){
+  var scaledArray = [];
+  var spaces = [];
+
+  // array = numberToCharacterMap(4);
+  array.forEach(function(line, i, mainArray){
+    if(line.indexOf('|') === -1 && line.indexOf('-') === -1) {
+      spaces = ' '.repeat(line.length + size -1);
+      scaledArray.push(spaces.split(""));
+    }
+    else if (line.indexOf('|') >= 0) {
+      var tempSize = size;
+      while (tempSize-1 > 0) {
+        line.splice(line.length-1, 0, ' ');
+        tempSize --;
+      }  
+      tempSize = size;
+      while(tempSize > 0) {
+        scaledArray.push(line);
+        tempSize --;
+      }
+    }
+    else if (line.indexOf('-') > 0) {
+      var tempSize = size;
+      while(tempSize-1 > 0) {
+        line.splice(line.length-1, 0, '-');
+        tempSize --;
+      }
+      scaledArray.push(line);
+    }
+  });
+
+  return scaledArray;
+}
+
+function displayScaledDigit(expandedCharacterArray) {
+  expandedCharacterArray.forEach(function(line){
+    var ch = line.join(" ");
+    console.log(ch);
+  });
+}
+
+var inputDigit = displayDigit(four);
+var scaledDigit = scale(four, 5);
+displayScaledDigit(scaledDigit);
+// var displayScaledDigit =  displayDigit(scaleDigit);
 
 // var digits = { 0 : [], 1 : [], 2: []}
 
