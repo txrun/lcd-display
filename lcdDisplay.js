eb28fce5ff5206ca381d3bf9081492d71849b879
Tarun Mathew <crushd@gmail.com>
2016-08-10 16:29:54 +0530

    Partially scaled the digit vertically

--- lcdDisplay.js
+++ lcdDisplay.js
@@ -71,28 +71,39 @@ const LCD = {
     ]
 };
 
-function display(key) {
-    var digit = LCD[key];
-    digit.forEach(function(line) {
-        console.log(line.join(""));
+function display(arrayInput) {
+        arrayInput.forEach(function(row) {
+        console.log(row.join(""));
     });
 }
-
 
 function scaleHorizontally(sizeOneDigit, sizeToScale) {
     return sizeOneDigit.map(function(row) {
-        var size = sizeToScale - 1;
-        return(row.slice(0, 2)        
-            .concat(row[1].repeat(size - 1).split(''))
-            .concat(row.slice(2)));
+        return row.slice(0, 2)        
+            .concat(row[1].repeat(sizeToScale - 1).split(''))
+            .concat(row.slice(2));
     });
 }
 
-for(var i = 0; i < 9; i++)
-{
+function scaleVertically(horizontallyScaledDigit, sizeToScale) {
+    return horizontallyScaledDigit.map(function(scaledRow, i, mainArray) {
+
+       
+        if( i === 3) {
+           while(sizeToScale-1 > 0) {
+                sizeToScale --;
+                display (mainArray.splice(3, 0, scaledRow));
+            } 
+        }
+    });
+}
+
+for(var i = 0; i < 9; i++) {
     var num = LCD[i];
-    var result = scaleHorizontally(num, 3);
+    var arrayResult = scaleHorizontally(num, 3);
+    scaleVertically(arrayResult, 3);
     console.log(i);
-    console.log(result);
+    console.log(arrayResult);
+    display(arrayResult);
     console.log(" ");
 }
